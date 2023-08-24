3d0678d4dc4d36db407a5a4e91d71632346fd7eb
Khaled Ahmed <khahmed2143@gmail.com>
2023-08-23 23:40:43 -0400

    V4

--- MVC/models.py
+++ MVC/models.py
@@ -13,9 +13,10 @@ class ArtWork(db.Model):
     description: str = db.Column(db.String(500), nullable=False)
     image: str = db.Column(db.String(100), nullable=False)
     title: str = db.Column(db.String(50), nullable=False)
-    price: float = db.Column(db.Float, nullable=False)
+    starting_price: float = db.Column(db.Float, nullable=False)
+    highest_bid: float = db.Column(db.Float, nullable=False)
     artist_id: int = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    
+
     #make get_art_work_by_id() method
 
 @dataclass
@@ -36,24 +37,8 @@ class Buyer(db.Model):
     buyer_id: int = db.Column(db.Integer, primary_key=True)
     username: str = db.Column(db.String(50), nullable=False)
     email: str = db.Column(db.String(50), nullable=False)
-    addressLine1: str = db.Column(db.String(50), nullable=False)
-    addressLine2: str = db.Column(db.String(50), nullable=True)
-    city: str = db.Column(db.String(50), nullable=False)
-    state: str = db.Column(db.String(50), nullable=False)
-    zipcode: str = db.Column(db.String(50), nullable=False)
+    phone_number: str = db.Column(db.String(50), nullable=False)
 
-
-@dataclass
-class Listings(db.Model):
-    __tablename__ = 'listing'
-    id: str = db.Column(db.Integer, primary_key=True)
-    artist_id: int = db.Column(db.Integer, db.ForeignKey('artist.id')) #FK to seller model (table)
-    title: str = db.Column(db.String(100))
-    image: str = db.Column(db.String(100)) #may use BLOB datatype
-    price: int = db.Column(db.Integer)
-    description: str = db.Column(db.String(1000))
-    listing_date: str = db.Column(db.String(100))
-    art_work_id: int = db.Column(db.Integer, db.ForeignKey('art_work.id')) #FK to art_work model (table)
 
 
 class Guest:
--- MVC/views.py
+++ MVC/views.py
@@ -4,13 +4,65 @@ from werkzeug.utils import secure_filename
 import os
 from MVC.models import Artist
 from MVC.models import ArtWork
+from MVC.models import Buyer
 
+def place_bid(artwork_id, buyer_id, bid_amount):
+    artwork = get_artwork_by_id(artwork_id)
+
+    if not artwork:
+        return jsonify({'message': 'Artwork not found!'})
+    
+    buyer = get_buyer_by_id(buyer_id)
+
+    if not buyer:
+        return jsonify({'message': 'Buyer not found!'})
+    
+    if bid_amount <= artwork.price:
+        return jsonify({'message': 'Bid amount should be greater than the current price!'})
+    
+    artwork.pprice = bid_amount
+    artwork.buyer = buyer
+    artwork.highest_bid = bid_amount
+
+
+# Get Requests
 
 def get_artist():
     return Artist.query.all()
 
 def get_artwork():
     return ArtWork.query.all()
+
+def get_buyer():
+    return Buyer.query.all()
+
+def get_artwork_by_id(id):
+    return ArtWork.query.filter_by(id=id).first()
+
+def get_artwork_by_artist(artist):
+    return ArtWork.query.filter_by(artist=artist).all()
+
+def get_artwork_by_title(title):
+    return ArtWork.query.filter_by(title=title).all()
+
+def get_artwork_by_price(price):
+    return ArtWork.query.filter_by(price=price).all()
+
+def get_artwork_by_creation_date(creation_date):
+    return ArtWork.query.filter_by(creation_date=creation_date).all()
+
+def get_artist_by_id(id):
+    return Artist.query.filter_by(id=id).first()
+
+def get_artist_by_name(name):
+    return Artist.query.filter_by(name=name).first()
+
+def get_buyer_by_id(id):
+    return Buyer.query.filter_by(id=id).first()
+
+def get_buyer_by_name(name):
+    return Buyer.query.filter_by(name=name).first()
+
 
 
 # Post Requests
@@ -50,7 +102,8 @@ def add_artwork():
 
     artwork = ArtWork(title=artwork_data['title'],
                       artist=artist, creation_date=date.today(),
-                      price=artwork_data['price'], 
+                      starting_price=artwork_data['price'], 
+                      highest_bid=artwork_data['price'],
                       description=artwork_data['description'], 
                       dimension=artwork_data['dimension'], 
                       image=artwork_data['image'])
@@ -60,3 +113,16 @@ def add_artwork():
     db.session.commit()
 
     return jsonify({'message': 'Artwork added successfully!'})
+
+# Add Buyer  
+def add_buyer():
+    buyer_data = request.get_json()
+
+    buyer = Buyer(username=buyer_data['name'], email=buyer_data['email'], phone_number=buyer_data['phone_number'])
+    
+    from MVC.models import db
+    db.session.add(buyer)
+    db.session.commit()
+
+    return jsonify({'message': 'Buyer added successfully!'})
+
